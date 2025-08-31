e999180b82f259f43ff9cfd39371e95bceab0edf
Lucas Felix <134363692+FelixLucasss@users.noreply.github.com>
2025-08-30 22:04:56 -0300

    att

--- Santander-2025-FrontEnd/Projetos/Portfolio2/assets/js/api.js
+++ Santander-2025-FrontEnd/Projetos/Portfolio2/assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/FelixLucasss/DIO/refs/heads/main/Santander-2025-FrontEnd/Projetos/Portfolio2/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
